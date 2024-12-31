85bfc2ad54c77ec070724968a69eaad4fc2c98cb
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2024-12-31 12:35:59 +0530

    Updated files

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import { useState } from "react";
 
-function BlogForm() {
+function BlogForm({ addBlog }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ((title, content)) {
+      addBlog(title, content);
+      setTitle("");
+      setContent("");
+    }
+  };
+
   return (
-    <div>
-      <input type="text" placeholder='Blog Title' />
-      
-    </div>
-  )
+    
+      <form className="blog-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Blog Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          value={content}
+          placeholder="Enter Content"
+          rows={5}
+          cols={30}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <button type="submit">Add Blog</button>
+      </form>
+    
+  );
 }
 
-export default BlogForm
+export default BlogForm;
